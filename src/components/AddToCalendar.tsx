a5609c1f649ebd54d0fe4b4f6b7c844abe188acc
Kyle Downs <mail@kyledowns.com>
2024-07-20 09:30:45 -0500

    fix date one more time

--- src/components/AddToCalendar.tsx
+++ src/components/AddToCalendar.tsx
@@ -14,8 +14,7 @@ export default function AddToCalendar() {
 
 async function createCalendarEvent() {
   const event: EventAttributes = {
-    start: weddingDate,
-    startInputType: "utc",
+    start: [2025, 6, 14, 17, 0],
     duration: { hours: 5, minutes: 0 },
     title: "Kyle and Amanda's Wedding",
     description: "Kyle and Amanda are getting married!",
