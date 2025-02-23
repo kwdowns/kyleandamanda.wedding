2b6a4b48fdf111d1e5f9e2fd68e0343e3e0f25b6
Kyle Downs <12downsk@gmail.com>
2025-02-23 16:49:39 -0600

    save form state to local storage

---
+++ src/components/Button.tsx
@@ -0,0 +1,32 @@
+
+export interface ButtonProps<T = any> {
+    text: string;
+    onClick?: () => Promise<T> | void;
+}
+export default function Button<T>({ text, onClick }: ButtonProps<T>) {
+    return (
+    <button
+        className="
+          shadow-md
+          visited:underline
+          bg-tertiary-light
+          hover:bg-tertiary-dark
+          border
+          border-black
+          rounded-md
+          inline-block
+          p-1
+          m-1
+          min-w-28
+          font-semibold
+        "
+        onClick={async () => {
+          if (onClick) {
+            await onClick();
+          }
+        }}
+      >
+        {text}
+      </button>
+    );
+}
