ffc6ac7c04c30c984964b995b6148a17c48d2741
Kyle Downs <12downsk@gmail.com>
2025-06-08 22:17:57 -0500

    simpler

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,13 +3,12 @@ import Link from "next/link";
 import Image from "next/image";
 import siteIcon from "@/../public/icon.png";
 import { Inter } from "next/font/google";
-import { rsvpFlag, travelFlag } from "../flags";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export  function NavBarLayout() {
-  const rsvpEnabled = rsvpFlag;
-  const travelEnabled = travelFlag;
+  const rsvpEnabled = false;
+  const travelEnabled = true;
   return (
     <nav
       className={`flex justify-around ${inter.className} text-black items-center`}
@@ -18,13 +17,9 @@ export  function NavBarLayout() {
         <Image src={siteIcon} alt="" width={40} />
       </Link>
       {rsvpEnabled && <Link href="/rsvp">RSVP</Link>}
-      {travelEnabled && (
-        <>
-          <Link href="/travel">Travel</Link>
-          <Link href="/registry">Gift Registry</Link>
-          <Link href="/faq">FAQ</Link>
-        </>
-      )}
+      {travelEnabled && <Link href="/travel">Travel</Link>}
+      {travelEnabled && <Link href="/registry">Registry</Link>}
+      {travelEnabled && <Link href="/faq">FAQ</Link>}
       <Link href="/gallery">Gallery</Link>
     </nav>
   );
